1e0d65cfb152b1e68f56dc28debf137ae9c09f1f
Gayane <ggrigoryan015ait@gmail.com>
2024-02-09 10:36:46 +0400

    Add project files.

---
+++ IMA2, Grigoryan Gayane, Homework 2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMA2__Grigoryan_Gayane__Homework_2
+{
+    internal class Program
+    {
+        //Task 1
+        /* static void MiddleElement(List<int> list)
+        {
+        int count = list.Count;
+        int middle1;
+        int middle2;
+        int i;
+        if (count % 2 != 0)
+        {
+            i = count / 2;
+            middle1 = list[i];
+            Console.WriteLine("The middle element is "+" "+middle1);
+        }
+        else {
+            i = count / 2;
+            middle1 = list[i-1];
+            middle2 = list[i];
+            Console.WriteLine("The middle elements are " + " " + middle1 + " and "+ middle2);
+          
+        }
+        }
+        static void Main(string[] args)
+        {
+        List<int> numbers = new List<int> { 1, 2, 3, 4, 5 };
+        MiddleElement(numbers);
+        }
+    */
+        //task2
+       /* static void MiddleElement(List<int> list)
+        {
+            int max = 0;
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i] > max)
+                {
+                    max = list[i];
+                }
+            }
+            Console.WriteLine("The largest element is" + " "+max);
+        }
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int> { 1, 2, 16, 4, 5 };
+            MiddleElement(numbers);
+        }
+       */
+        
+    }
+}
